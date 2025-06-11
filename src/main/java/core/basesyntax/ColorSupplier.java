a78d96b8498199b12d936967723a0198703e9413
Dmytro Smercuk <Dmitryvin1992@gmail.com>
2025-06-11 14:48:18 +0300

    Initial commit from local project-2

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private final Random random = new Random();
+
     public Colors getRandomColor() {
 
         Colors[] colors = Colors.values();
