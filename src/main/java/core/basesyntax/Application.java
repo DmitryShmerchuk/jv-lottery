0669b4bfd68f169b903116172ffc5876a0ef76fc
Dmytro Smercuk <Dmitryvin1992@gmail.com>
2025-06-12 14:20:01 +0300

    Initial commit from local project-6

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        int numberOfBalls = 3;
+        final int numberOfBalls = 3;
 
         for (int i = 0; i < numberOfBalls; i++) {
             System.out.println(lottery.getRandomBall());
