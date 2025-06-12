549deeedc5b34e56543336b33a0e3aebb70e6e16
Dmytro Smercuk <Dmitryvin1992@gmail.com>
2025-06-12 14:31:31 +0300

    Initial commit from local project-7

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,15 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private static int MAX_NUMBER = 100;
-    private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int MAX_NUMBER = 100;
 
     public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
-        String randomColor = colorSupplier.getRandomColor().toString();
-        int randomNumber = random.nextInt(MAX_NUMBER) + 1;
-
-        return new Ball(randomColor, randomNumber);
+        String color = colorSupplier.getRandomColor();
+        int number = new Random().nextInt(MAX_NUMBER);
+        return new Ball(color, number);
     }
 }
